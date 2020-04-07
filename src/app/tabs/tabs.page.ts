71b5abb7d61f466c099b3c3044f50399732738b5
Alicia García <agc162@alu.ua.es>
2020-04-07 22:17:30 +0200

    Practica 1. Salir de la App, con confirmación

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(
+    public alertController: AlertController,
+    public platform: Platform,
+    ) { this.platform = platform; }
 
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure you want to go out??',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            this.closed();
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  public closed() {
+    this.platform.backButton.subscribe(() => {
+     console.log ('exit');
+     navigator['app'].exitApp();
+ });
+}
 }

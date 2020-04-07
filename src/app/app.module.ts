130d4cca2a938dff7c62a49cdf99419136ed6583
Alicia García <agc162@alu.ua.es>
2020-04-07 21:04:03 +0200

    Practica 1. Toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -20,4 +20,33 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private toastCtrl: ToastController
+  ) {
+    this.presentToast();
+  }
+
+
+  async presentToast() {
+    const toast = await this.toastCtrl.create({
+      header: 'HeartStoneApp',
+      position: 'middle',
+      color: 'medium',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: '  Welcome!!'
+        }, {
+          text: 'Close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Closed');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+}
